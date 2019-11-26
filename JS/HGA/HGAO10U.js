57c55761a72dc92b5fcf03e4886b7dbc2a528361
Rijk van Putten <rijkvanputten@hotmail.nl>
2019-11-26 09:30:06 +0000

    Opdr 10

--- JS/HGA/HGAO10U.js
+++ JS/HGA/HGAO10U.js
@@ -2,9 +2,17 @@ var achtergrond;
 var flatboyX = -140;
 var flatboyBreedte = 258;
 var flatboyHoogte = 237;
+var animatie = [];
+var aantalFrames = 15;
+var huidigeFrame = 0;
 
 function preload() {
   achtergrond = loadImage("images/backgrounds/green_grass_landscape.svg");
+  for(var i = 1; i <= aantalFrames; i++)
+  {
+      nieweFrame = loadImage("images/sprites/flatboy/Jump(" + i + ").png");
+      animatie.push(nieweFrame);
+  }
 }
 
 function setup() {
@@ -14,6 +22,14 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-  background(achtergrond);
+    background(255);
+    background(achtergrond);
+    image(animatie[huidigeFrame], flatboyX, 10, flatboyBreedte, flatboyHoogte);
+    huidigeFrame++;
+    huidigeFrame %= aantalFrames;
+    flatboyX += 5;
+    if (flatboyX >= 750)
+    {
+        flatboyX = -140;
+    }
 }
