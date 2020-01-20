eacd9959b9ec765453ca641239c522cc394d974a
Rijk van Putten <rijkvanputten@hotmail.nl>
2020-01-20 07:55:13 +0000

    PW TEST

--- JS/test.js
+++ JS/test.js
@@ -1,24 +1,24 @@
+var rotation = 0;
+
 function setup() {
-  canvas = createCanvas(500, 500);
-  background('lightgrey');
-  canvas.parent('processing');
-  //noLoop();
+    var canvas = createCanvas(1000, 900);
+    canvas.parent('processing');
+    angleMode(DEGREES); 
+    frameRate(15);
 }
 
 function draw() {
-    noStroke();
-
-    // De cirkel
-    fill(175, 150, 150);
-    ellipse(0 + (176 / 2), 500 - (176 / 2), 176);
-
-    // Het vierkant
-    stroke(125, 75, 75);
-    strokeWeight(40);
-    rect(500 - 225 - 20, 0 + 20, 225, 255);
-
-    // De transparante driehoek\
-    noStroke();
-    fill(255, 255, 0, 255/2);
-    triangle(0, 200, 500, 0, 350, 500);
+    background('grey');
+    rotation++;
+    if(!keyIsDown(DOWN_ARROW))
+    {
+        rotation = constrain(rotation, 0, 10);
+    }
+    push();
+    fill(random(255), random(255), random(255));
+    rotate(rotation);
+    rectMode(CENTER);
+    scale(2);
+    rect(300, 100, 100, 100);
+    pop();
 }
